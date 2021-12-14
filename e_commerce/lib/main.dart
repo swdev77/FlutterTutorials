279c293b7768ad6ca4ac4d04788bf61d6edbb4d8
swdev77 <swdev77@gmail.com>
2021-12-14 18:09:59 +0500

    E-Commerce App UI: initial commit

---
+++ e_commerce/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:e_commerce/constants.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Muli',
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(color: kTextColor),
+          bodyText2: TextStyle(color: kTextColor),
+        ),
+      ),
+      home: Scaffold(),
+    );
+  }
+}
