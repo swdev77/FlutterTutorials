c3b198c0b5f78782e4ee621364e0bbb4f96cb831
swdev77 <swdev77@gmail.com>
2021-12-26 19:50:00 +0500

    Chatting app w/ themes: Welcome screen

---
+++ chatting_app_ui_theming/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'theme.dart';
+import 'screens/welcome/welcome_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat App Demo',
+      debugShowCheckedModeBanner: false,
+      theme: lightThemeData(context),
+      darkTheme: darkThemeData(context),
+      themeMode: ThemeMode.system,
+      home: const WelcomeScreen(),
+    );
+  }
+}
